205962925128ce38bbf058bbc5f0eff2dfd2bfa2
hasc <harald.schlindwein@se.com>
2019-01-21 12:25:50 +0000

    adding whois functionality

--- commands/whois.js
+++ commands/whois.js
@@ -1,8 +1,36 @@
+const jsonfile = require('jsonfile')
+const nicksFile = './data/nicks.json'
+const usrPrefix = '@';
+
+
 function whois(msg, args) {
 	if (msg === null) return;
-	msg.author.createDM()
+	var replyMsg = '';
+	if (args === null || !(args instanceof Array) || args.length == 0 || !args[0].startsWith(usrPrefix)) {
+			replyMsg = 'No user provided for whois. Usage !whois <@user>.';
+			respond(msg, replyMsg);
+			return;
+	}
+	jsonfile.readFile(nicksFile)
+		.then(nicks => {
+			var usr = args[0].substring(1);
+			if (nicks.hasOwnProperty(usr)) {
+				replyMsg = `${usr}'s ingame nick is ${nicks[usr]}`;
+			}
+			else {
+				replyMsg = `No ingame nick found for ${usr}`;
+			}
+			respond(msg, replyMsg);
+			return;
+		})
+		.catch(console.error) 	
+}
+
+function respond(receivedMsg, response) {
+	receivedMsg.author.createDM()
 		.then((dmChannel) => {
-			dmChannel.send(`You called WHOIS with ${args}`)
+			response = response || 'Something went wrong, please try again!';
+			dmChannel.send(response)
 				.catch(console.error);
 		})
 		.catch(console.error);
