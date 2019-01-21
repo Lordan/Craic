18643ee118289508082c02cd1e770fd774be4c74
hasc <harald.schlindwein@se.com>
2019-01-21 18:06:38 +0000

    removing guild/dm check, improving logging

--- craic_me_up.js
+++ craic_me_up.js
@@ -32,7 +32,7 @@ client.on('message', msg => {
     var cmd = args[0];
 
     args = args.splice(1);
-	console.log(`Command received, message.guild: ${msg.guild}`);
+	console.log(`Command ${cmd} received, message.guild: ${msg.guild}, args: ${JSON.stringify(args)}`);
 	const guild = msg.guild === null ? 'Craic' : msg.guild;
     switch(cmd.toLowerCase()) {
         case 'ping':
