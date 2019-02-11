08308a61c7e10fe57e1b557893ae4b6e1b6c0732
hasc <harald.schlindwein@se.com>
2019-02-11 12:58:43 +0000

    fixing column typo

--- tools/dbQueries.js
+++ tools/dbQueries.js
@@ -3,7 +3,7 @@ exports.userQueries = {
 	'getIngameNickByDiscordId'	: 	'SELECT u.ingame_nick FROM users AS u WHERE u.discord_id = $1',
 	'getUsernameByIngameNick'	: 	'SELECT u.user_name FROM users AS u WHERE u.ingame_nick = $1',
 	'getUsernameByUserId'		: 	'SELECT u.user_name FROM users AS u WHERE u.user_id = $1',
-	'getUserIdByDiscordId'		:	'SELECT u.id FROM users AS u WHERE u.discord_id = $1',
+	'getUserIdByDiscordId'		:	'SELECT u.id FROM users AS u WHERE id = $1',
 	'setIngameNick'				: 	'INSERT INTO users (discord_id, user_name, ingame_nick, guild) VALUES ($1, $2, $3, $4) ON CONFLICT (discord_id) DO UPDATE SET ingame_nick = EXCLUDED.ingame_nick'
 }
 
