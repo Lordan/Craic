5d72d2a3440ea2870f38e80ef066da8fcefe3595
hasc <harald.schlindwein@se.com>
2019-02-11 15:02:19 +0000

    adding missing ,

--- tools/dbQueries.js
+++ tools/dbQueries.js
@@ -14,7 +14,7 @@ exports.statsQueries = {
 exports.tileQueries = {
 	'setTile'					:	'INSERT INTO tiles (user_id, tile, reported) VALUES ($1, $2, NOW())',
 	'getTile'					:	'SELECT t.user_id FROM tiles AS t WHERE t.tile = $1',
-	'clearTile'					:	'DELETE FROM tiles WHERE tiles.user_id = $1 AND tiles.tile = $2'
-	'clearAllTiles'					:	'DELETE FROM tiles'
+	'clearTile'					:	'DELETE FROM tiles WHERE tiles.user_id = $1 AND tiles.tile = $2',
+	'clearAllTiles'				:	'DELETE FROM tiles'
 }
 
